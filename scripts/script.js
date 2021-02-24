29448d5ed8013b8848e48ac98c34cc692e5bc45f
AhmadRaoofYT <64564404+AhmadRaoofYT@users.noreply.github.com>
2021-02-24 18:28:55 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,66 @@
+function clik1() {
+    window.location.href="pages/page1.htm"
+    
+}
+function clik() {
+    window.location.href="../index.html"
+    
+}
+function clik2() {
+    window.location.href="page2.html"
+    
+}
+function login() {
+    window.location.href="../index-loged-in.html"
+
+}
+function required()
+{
+var empt = document.form1.text1.value;
+if (empt === "")
+{
+alert("Please input a Value");
+return false;
+}
+else 
+{
+alert('Login successful');
+return true;
+
+}
+}
+function required()
+{
+var empt = document.form1.text2.value;
+if (empt === "")
+{
+alert("Please input a Value");
+return false;
+}
+else 
+{
+window.location.href="../index-loged-in.html";
+return true;
+}
+}
+function hello() {
+{
+var empt = document.form1.text1.value;
+if (empt === "User")
+{
+    window.location.href="../index-loged-in.html"
+return true
+}
+}
+}
+function hello() {
+    {
+    var empt = document.form1.text2.value;
+    if (empt === "User")
+    {
+        window.location.href="../index-loged-in.html"
+    return true
+    }
+    }
+    }
+    
---
+++ test.js
@@ -0,0 +1,21 @@
+function hello() {
+    {
+    var empt = document.form1.text1.value;
+    if (empt === "User")
+    {
+        window.location.href="../index.html"
+    return true
+    }
+    }
+    }
+function hello() {
+    {
+    var empt = document.form1.text2.value;
+    if (empt === "Password")
+    {
+        window.location.href="../index.html"
+    return true
+    }
+    }
+    }
+        
